7eec975b03916c07baf9d04c1ca25966102af1bd
hannahGu <1065316432@qq.com>
2017-12-07 18:59:04 +0800

    merge

--- src/index.js
+++ src/index.js
@@ -22,6 +22,9 @@ import GraphChart from './Echarts/GraphChart';
 import KLineChart from './Echarts/KLineChart';
 
 import ScatterNet from './Echarts/ScatterNet';
+// WordCloud
+import WordCloud from './Echarts/WordCloud';
+
 // map
 // import MapBasic from './Echarts/MapBasic';
 import BaiduMapPoint from './Map/BaiduMapPoint';
@@ -42,6 +45,7 @@ import ScatterCartesian from './Echarts/ScatterCartesian';
 // Pictogram start
 import ImageBar from './Pictogram/ImageBar';
 import ImagePercent from './Pictogram/ImagePercent';
+import ProgressBar from './Pictogram/ProgressBar';
 
 // canvas
 
@@ -91,8 +95,10 @@ module.exports = {
   MapScatter,
   ShadeMapScatter,
   ScatterCartesian,
+  WordCloud,
   ImageBar,
   ImagePercent,
+  ProgressBar,
   RainbowRain,
   Gauge,
   GaugePan,
